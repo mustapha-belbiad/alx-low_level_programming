bf48a5f5b950ee322449fee418ed04b1b3770c63
mustapha-belbiad <mustapha.belbiad10@gmail.com>
2023-03-16 16:38:50 -0700

    .

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/*
+ * main - programe print a line with printf function 
+ * Return: 0 (success)
+*/
+
 int main(void)
+
 {
-printf("%s", "with proper grammar, but the outcome is a piece of art,");
-return (0);
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+
 }
