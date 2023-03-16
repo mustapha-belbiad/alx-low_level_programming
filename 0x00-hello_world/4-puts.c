7f087759c955920a54a85763a9219d0053f98541
mustapha-belbiad <mustapha.belbiad10@gmail.com>
2023-03-16 16:34:25 -0700

    .

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,13 @@
 
 /**
  * main - program that print a message with puts function
- * return: 0 (success)
+ * Return: 0 (success)
 */
 
 int main(void)
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }
