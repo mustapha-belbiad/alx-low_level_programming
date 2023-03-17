f5fbfe8600a16993c7bc5fc46f87a788ef12a804
mustapha-belbiad <mustapha.belbiad10@gmail.com>
2023-03-16 17:28:25 -0700

    .

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,23 +2,22 @@
 
 /**
  *  main - program print size data type
- *  Return: 0
+ *  Return:0 
  */
 
 int main(void)
-
 {
-	char	a;
-	int	b;
+	char a;
+	int b;
 	long int c;
 	long long int d;
-	float	e;
+	float f;
 
-	printf("Size of a char: %zu byte(s)\n", sizeof(a));
-	printf("Size of an int: %zu byte(s)\n", sizeof(b));
-	printf("Size of a long int : %zu byte(s)\n", sizeof(c));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(d));
-	printf("Size of a float: %zu byte(s)\n", sizeof(e));
+	printf("size of a char: %d byte(s)\n", (int) sizeof(a));
+	printf("size of a int: %d byte(s)\n", (int) sizeof(b));
+	printf("size of a long int: %d byte(s)\n", (int) sizeof(c));
+	printf("size of a lolng lolng int: %d byte(s)\n", (int) sizeof(d));
+	printf("size of a float: %d byte(s)\n", (int) sizeof(f));
 	return (0);
 
 }
