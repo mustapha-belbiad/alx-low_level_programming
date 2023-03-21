e0daf455ed1152acc8582631a4ecd9f43ade181c
mustapha-belbiad <mustapha.belbiad10@gmail.com>
2023-03-20 17:36:51 -0700

    .

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints tha alphabet in lowercase
+ *
+ * Return: 0
+*/
+
+int main(void)
+
+{
+	char i = 'a';
+
+	while (i <= 'z')
+		putchar(i++);
+
+	putchar('\n');
+
+	return (0);
+}
