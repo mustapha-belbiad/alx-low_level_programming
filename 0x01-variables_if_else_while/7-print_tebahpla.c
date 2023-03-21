74c5dc04badb08871534bb23f3dceb5b2473ab09
mustapha-belbiad <mustapha.belbiad10@gmail.com>
2023-03-20 17:57:36 -0700

    .

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print the lowercase alphabet in reverse.
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int str;
+
+	for  (str = 'z'; str >= 'a'; str--)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
